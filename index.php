7ba9faa6d7ce9b6d28b143c2f0075124517aa30f
Carminelm <carminelm54@gmail.com>
2024-09-04 17:38:51 +0200

    upgatee index

--- index.php
+++ index.php
@@ -18,6 +18,16 @@ require_once __DIR__ . '/catdogs/catdogs.php';
     <title>Document</title>
 </head>
 <body>
+<?php foreach($products as $product): ?>
+            <div>
+                 <h1> Nome : <?php echo  $product->name  ?></h1>
+                 <h2>Tipo : <?php echo  $product->type ?></h2>
+                 <h3>Prezzo : <?php echo  $product-> price ?> </h3>
+                 <h4>Descrizione :<?php echo  $product-> description ?> </h4>
+                 <h5>Only :<?php echo  $product-> only ?? '-' ?> </h5>
+                             
+            </div>
+        <?php endforeach; ?>
     
 </body>
 </html>
